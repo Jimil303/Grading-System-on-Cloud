5d483c67b7ca2ee45978c45f2fa5ee755490b5a6
Jimil303 <jimilprajapati@gmail.com>
2021-06-19 03:43:15 +0300

    changes

--- grading/views.py
+++ grading/views.py
@@ -10,5 +10,15 @@ def index(request):
 def reguser(request):
     return render(request,'regUser.html')
 
+def search(request):
+    return render(request,'search.html')
+
+def notification(request):
+    return render(request,'notifications.html')
+
+def sendreq(request):
+    return render(request,'sendReq.html')
+
+
 
 # Create your views here
