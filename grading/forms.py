b427afc3cb3cdb2f60a190ecf8368f84290de7e1
Jimil303 <jimilprajapati@gmail.com>
2021-07-03 02:50:54 +0300

    notification page added ( still need to work on the table)

--- grading/forms.py
+++ grading/forms.py
@@ -5,15 +5,11 @@ class Myform (forms.ModelForm):
  class Meta:
         model = FacultyCredentials
         fields = ['username', 'password', 'university', 'auth',]
-<<<<<<< Updated upstream
         exclude =[]
         labels = {'username': "Username", 'password': "Password", 'university': "University", 'auth': "Infoadded"}
 class Myform2 (forms.ModelForm):
  class Meta:
         model = StudentCredentials
         fields = ['username', 'password', 'university', 'auth',]
-=======
->>>>>>> Stashed changes
         exclude =[]
         labels = {'username': "Username", 'password': "Password", 'university': "University", 'auth': "Infoadded"}
-    
