7ea06545b0adfc4dd5acba12058305faa2aa1d08
Jimil303 <jimilprajapati@gmail.com>
2021-07-10 13:05:03 +0300

    changes

--- grading/forms.py
+++ grading/forms.py
@@ -3,18 +3,18 @@ from django.db.models import fields
 from grading.models import FacultyCredentials,StudentCredentials, course, messenger, semester
 
 class Myform (forms.ModelForm):
- class Meta:
-        model = FacultyCredentials
-        fields = ['fac_id','name','phone','email','username', 'password']
-        exclude =[]
-        labels = {'fac_id':"Faculty ID",'name':"Full Name",'phone':"Phone no",'email':"e-mail",'username': "Username", 'password': "Password"}
+       class Meta:
+              model = FacultyCredentials
+              fields = ['fac_id','name','phone','email','username', 'password']
+              exclude =[]
+              labels = {'fac_id':"Faculty ID",'name':"Full Name",'phone':"Phone no",'email':"e-mail",'username': "Username", 'password': "Password"}
 
 class Myform2 (forms.ModelForm):
- class Meta:
-        model = StudentCredentials
-        fields = ['stu_id','name','phone','email','username', 'password']
-        exclude =[]
-        labels = {'stu_id':"Student ID",'name':"Full Name",'phone':"Phone no",'email':"e-mail",'username': "Username", 'password': "Password"}
+       class Meta:
+              model = StudentCredentials
+              fields = ['stu_id','name','phone','email','username', 'password']
+              exclude =[]
+              labels = {'stu_id':"Student ID",'name':"Full Name",'phone':"Phone no",'email':"e-mail",'username': "Username", 'password': "Password"}
 
 class Myform3 (forms.ModelForm):
        class Meta:
@@ -24,22 +24,12 @@ class Myform3 (forms.ModelForm):
               labels = {'name' : "Course Name",'code':"Course Code"}
 
 class Myform4(forms.ModelForm):
-<<<<<<< Updated upstream
        year = forms.CharField(label='', 
               widget=forms.TextInput(attrs={'placeholder': 'Year'}))
        type = forms.CharField(label='', 
               widget=forms.TextInput(attrs={'placeholder': 'Semester Name'}))
        number = forms.CharField(label='', 
               widget=forms.TextInput(attrs={'placeholder': 'Semester Number'}))
-=======
-       
-       year = forms.CharField(label='', 
-              widget=forms.TextInput(attrs={'placeholder': 'Year'}))
-       type = forms.CharField(label='', 
-              widget=forms.TextInput(attrs={'placeholder': 'Enter Semester name'}))
-       number = forms.CharField(label='', 
-              widget=forms.TextInput(attrs={'placeholder': 'Enter Sem Number'}))
->>>>>>> Stashed changes
        class Meta:
               model = semester
               fields = ['year','type','number']
